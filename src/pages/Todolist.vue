866a3325a3ac3bb6174db72924dbfe92c72ef8c0
liwanzhen <2274036472@qq.com>
2018-04-17 18:15:59 +0800

    some modify

--- src/pages/Todolist.vue
+++ src/pages/Todolist.vue
@@ -6,21 +6,27 @@
       <button @click="addNewItem(newItem)">确认</button>
       <ul>
       <h2>您的代办事项：</h2>
-        <a href="javascript:;" v-on:click="deleteAll">删除所有</a>
+        <a href="javascript:;" v-on:click="deleteAll" class="el-icon-delete">删除所有</a>
         <li v-for="item in items" :key="item.label"  v-bind:class="{finished:item.isFinished}">
           {{ item.label }}
-          <a href="javascript:;" v-on:click="deleteOne">删除</a>
+          <a href="javascript:;" v-on:click="deleteOne"  class="el-icon-delete"></a>
         </li>
       </ul>
     </div>
-    <template>
+
       <div>
         <i class="el-icon-edit"></i>
         <i class="el-icon-share"></i>
         <i class="el-icon-delete"></i>
         <el-button type="primary" icon="el-icon-search"><i class="el-icon-delete"></i>搜索</el-button>
       </div>
-    </template>
+
+<el-button type="primary" icon="el-icon-edit"></el-button>
+<el-button type="primary" icon="el-icon-share"></el-button>
+<el-button type="primary" icon="el-icon-delete"></el-button>
+<el-button type="primary" icon="el-icon-search">搜索</el-button>
+<el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+
 
   </div>
 </template>
