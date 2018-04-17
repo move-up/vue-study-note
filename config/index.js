a9448c6aeb373a1f172ea551b80b00fce8190702
liwanzhen <2274036472@qq.com>
2018-04-17 12:54:57 +0800

    change pah

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist',
+    assetsPublicPath: '/vue-study-note/dist',
 
     /**
      * Source Maps
